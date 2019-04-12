8148f83831dc7182f4f8fd9c4fcd9165083da8c7
Chris Spring [CTR] <CSpring@corp.auction.local>
2019-04-12 14:40:38 -0500

    added shout out to dinesh

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.json({status: 200, message:'Hello Kubectl Services'});
+  res.json({status: 200, message:'Hello Kubectl Services, and Dinesh is cool!'});
 });
 
 app.listen(PORT, HOST);
